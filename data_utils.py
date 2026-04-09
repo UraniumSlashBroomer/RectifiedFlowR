fixed a lot of bugs. fixed: attention, image scaling, time embedding

data_utils.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 import os
+import torch
 from torch.utils.data import DataLoader
 
 def load_CIFAR_batch(filename):
@@ -26,7 +27,7 @@ def load_CIFAR10(root):
     Xte, Yte = load_CIFAR_batch(os.path.join(root, "test_batch"))
     return Xtr, Ytr, Xte, Yte
 
-def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000, subtract_mean=True):
+def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000, dtype=torch.float32):
     cifar10_dir = os.path.join(
             os.path.dirname(__file__), "dataset/cifar-10-batches-py"
     )
@@ -41,20 +42,20 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000, sub
     mask = [i for i in range(num_test)]
     X_test = X_test[mask]
     y_test = y_test[mask]
-
-    if subtract_mean:
-        mean_image = np.mean(X_train, axis=0)
-        X_train -= mean_image
-        X_val -= mean_image
-        X_test -= mean_image
+    
+    X_train, X_val, X_test = X_train / 255, X_val / 255, X_test / 255 # pixels are in [0, 1] range
+    
+    X_train = 2 * X_train - 1 # [-1, 1]
+    X_val = 2 * X_val - 1
+    X_test = 2 * X_test - 1
 
     result_dict = {
-        "X_train": X_train,
-        "y_train": y_train,
-        "X_val": X_val,
-        "y_val": y_val,
-        "X_test": X_test,
-        "y_test": y_test
+        "X_train": torch.tensor(X_train, dtype=dtype),
+        "y_train": torch.tensor(y_train, dtype=dtype),
+        "X_val": torch.tensor(X_val, dtype=dtype),
+        "y_val": torch.tensor(y_val, dtype=dtype),
+        "X_test": torch.tensor(X_test, dtype=dtype),
+        "y_test": torch.tensor(y_test, dtype=dtype)
     }
 
     return result_dict
@@ -64,3 +65,4 @@ if __name__ == '__main__':
     batch_size = 16
     dataloader = DataLoader(result_data["X_train"], batch_size=batch_size, shuffle=True, drop_last=True)
     print(f"CIFAR loaded correctly, num of samples when batch_size={batch_size}: {len(dataloader) * 16}")
+    print(f"pixel range {result_data['X_train'].min()}-{result_data['X_train'].max()}")

main.py
@@ -17,15 +17,18 @@ if __name__ == '__main__':
                              num_layers=4).to(device)
     
     data_dict = get_CIFAR10_data()
-    batch_size = 32 
-    epochs = 10 
+    batch_size = 32
+    epochs = 20
     optimizer = torch.optim.Adam(lr=3e-4, params=model.parameters(), weight_decay=1e-5)
     loss_instance = torch.nn.MSELoss()
     data_loader = torch.utils.data.DataLoader(data_dict["X_train"], batch_size=batch_size, shuffle=True, drop_last=True)
 
     train_rectified_flow_model(model=model, optimizer=optimizer, epochs=epochs, criterion=loss_instance, data_loader=data_loader, device=device)
 
-    generated_img = sample(model=model, B=1, T=10, device=device).cpu().squeeze(0).permute(1, 2, 0).detach().clamp_(min=0, max=1).numpy()
+    generated_img = sample(model=model, B=1, T=10, device=device).cpu().squeeze(0).permute(1, 2, 0).detach().numpy()
+    generated_img = (generated_img + 1) / 2
+    generated_img = np.clip(generated_img, a_min=0, a_max=1)
+
     plt.imsave('images/generated_image.png', generated_img)
 
         

rectified_flow.py
@@ -2,15 +2,15 @@ from models import *
 import utils
 import torch
 import torch.nn as nn
+import math
 
 class TimeEmbedding(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
-        sincosemb = torch.zeros(size=(1, 1, emb_dim))
-        i = torch.arange(0, emb_dim, step=2)
-        sincosemb[:, :, i] = torch.sin(10000 ** (-i / emb_dim))
-        i += 1
-        sincosemb[:, :, i] = torch.cos(10000 ** (-i / emb_dim)) # [1, 1, E]
+        half_dim = emb_dim // 2
+        sincosemb = math.log(10000) / (half_dim - 1)
+        sincosemb = torch.exp(torch.arange(half_dim) * -sincosemb)
+        sincosemb = sincosemb.reshape(1, 1, half_dim)
         
         self.fc1 = nn.Linear(emb_dim, emb_dim)
         self.SiLU = nn.SiLU()
@@ -23,7 +23,8 @@ class TimeEmbedding(nn.Module):
         t: [B, 1]
         output: 
         """
-        t = t * self.sincosemb # [1, 1, E]
+        t = t * self.sincosemb # [1, 1, E // 2]
+        t = torch.cat((t.sin(), t.cos()), dim=-1)
         t = self.fc1(t)
         t = self.SiLU(t)
         t = self.fc2(t)
@@ -63,7 +64,7 @@ class AdaLN_EncoderLayer(nn.Module):
 
         shortcut = x
         x = self.norm_self(x)
-        x = (1 + shift_a) * x + scale_a
+        x = (1 + scale_a) * x + shift_a
         x = self.self_attn(x, x, x)
         x *= gate_a
         x = self.dropout_self(x)
@@ -71,7 +72,7 @@ class AdaLN_EncoderLayer(nn.Module):
 
         shortcut = x
         x = self.norm_ffn(x)
-        x = (1 + shift_ffn) * x + scale_ffn
+        x = (1 + scale_ffn) * x + shift_ffn
         x = self.ffn(x)
         x *= gate_ffn
         x = self.dropout_ffn(x)